1fe55225cc0861420a5fa8c69bf8cb429eaaf384
Matias c <106419908+Matichz@users.noreply.github.com>
2023-01-16 11:05:42 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const boton_open_mobile = document.getElementById('btn-open');
 const boton_close_mobile = document.getElementById('btn-close');
+const navbar_mobile = document.getElementById('navegation-mobile');
 
 boton_open_mobile.addEventListener('click', () => {
-    document.querySelector('.menu-navegacion-bg').style.display = 'block';
+    navbar_mobile.classList.remove ('navbar-mobile');
 });
 
 boton_close_mobile.addEventListener('click', () => {
-    document.querySelector('.menu-navegacion-bg').style.display = 'none';
+    navbar_mobile.classList.add ('navbar-mobile');
 });
